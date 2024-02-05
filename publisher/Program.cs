d407eed3f5dcfdc7ee7fb2e5261238e838c443a3
Junghwan Bae <quiett20@gmail.com>
2024-02-05 14:14:54 +0900

    first

---
+++ publisher/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace csharpeventhub
+ {
+     using System;
+     using System.Text;
+     using System.Threading.Tasks;
+     using Microsoft.Azure.EventHubs;
+
+     
+     public class Program
+     {
+         private static EventHubClient eventHubClient;
+         private const string EventHubNamespace = "kedademoeh";
+         private const string EventHubName = "testhub";
+         private const string EventHubKey = "TaaQ7k/IYj0jyRuUlxGNf60LTqQtmETq5+AEhHKQf6w=";
+         private const string EventHubConnectionString = "Endpoint=sb://{0}.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey={1}";
+
+         public static void Main(string[] args)
+         {
+             MainAsync(args).GetAwaiter().GetResult();
+         }
+
+         private static async Task MainAsync(string[] args)
+         {
+             // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+             // Typically, the connection string should have the entity path in it, but for the sake of this simple scenario
+             // we are using the connection string from the namespace.
+             var connectionStringBuilder = new EventHubsConnectionStringBuilder(String.Format(EventHubConnectionString, EventHubNamespace, EventHubKey))
+             {
+                 EntityPath = EventHubName
+             };
+
+             eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            // Sending 50 messages to Event Hub
+             await SendMessagesToEventHub(10);
+             await eventHubClient.CloseAsync();
+
+             Console.WriteLine("Press ENTER to exit.");
+             Console.ReadLine();
+         }
+
+         // Creates an event hub client and sends 100 messages to the event hub.
+         private static async Task SendMessagesToEventHub(int numMessagesToSend)
+         {
+             for (var i = 0; i < numMessagesToSend; i++)
+             {
+                 try
+                 {
+                     var message = $"Message {i}";
+                     Console.WriteLine($"Sending message: {message}");
+                     await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                 }
+                 catch (Exception exception)
+                 {
+                     Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                 }
+
+                 await Task.Delay(10);
+             }
+
+             Console.WriteLine($"{numMessagesToSend} messages sent.");
+         }
+     }
+ }
